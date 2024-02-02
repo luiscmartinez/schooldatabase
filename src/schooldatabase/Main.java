cb1c1a7484d6cd451cbd23e041cd48fa3e40803e
Luis Martinez <mrluismartinezzz@gmail.com>
2024-02-02 15:25:20 -0800

    Main successfully renders add student view

--- src/schooldatabase/Main.java
+++ src/schooldatabase/Main.java
@@ -1,95 +1,67 @@
 package schooldatabase;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.util.Arrays;
 
 public class Main extends Application {
-    // Create the Message Label
-    Label messageLbl = new Label("Press any Menu Item to see the message");
 
     @Override
-    public void start(Stage stage) throws Exception {
-        // create some menus
-        Menu studentMenu = new Menu("Student");
-        Menu courseMenu = new Menu("Course");
-        // Create the student MenuItems
-        MenuItem addStudentItem = new MenuItem("Add Student");
-        MenuItem editStudentItem = new MenuItem("Edit Student");
-        MenuItem viewStudentItem = new MenuItem("View Student");
-        // Create the course MenuItems
-        MenuItem addCourseItem = new MenuItem("Add Course");
-        MenuItem editCourseItem = new MenuItem("Edit Course");
-        MenuItem viewCourseItem = new MenuItem("View Course");
-        addStudentItem.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Add Student");
-                // printMessage("You have pressed the New Menu Item");
-                // CreateAddStudentScene(primaryStage);
-            }
-        });
-        studentMenu.getItems().addAll(addStudentItem, editStudentItem, viewStudentItem);
-        courseMenu.getItems().addAll(addCourseItem, editCourseItem, viewCourseItem);
-        stage.setTitle("School Database");
-        // Create a menu bar
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("School Database");
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 400, 300);
+
+        // Create a MenuBar and add it to the top of the root pane
         MenuBar menuBar = new MenuBar();
-        // Add menus to a menu bar
-        menuBar.getMenus().addAll(studentMenu, courseMenu);
-        // Create the Menu Box
-        HBox menu = new HBox();
-        // Add the MenuBar to the Menu Box
-        menu.getChildren().add(menuBar);
+        root.setTop(menuBar);
 
-        // Create the VBox
-        VBox root = new VBox();
-        // Add the children to the VBox
-        root.getChildren().addAll(menu, messageLbl);
-        // Set the Size of the VBox
-        root.setMinSize(350, 250);
+        // Create the "student" menu using MenuFactory
+        Menu studentMenu = MenuFactory.createMenu("Student");
 
-        /*
-         * Set the padding of the VBox
-         * Set the border-style of the VBox
-         * Set the border-width of the VBox
-         * Set the border-insets of the VBox
-         * Set the border-radius of the VBox
-         * Set the border-color of the VBox
-         */
-        root.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");
+        // Create "addStudent" MenuItem with an action
+        MenuItem addStudent = MenuFactory.createMenuItem("Add Student", () -> showFormDialog());
 
-        // Create the Scene
-        Scene scene = new Scene(root);
-        // Add the scene to the Stage
-        stage.setScene(scene);
-        // Set the title of the Stage
-        stage.setTitle("School Database");
-        // Display the Stage
-        stage.show();
+        // Create "Edit" MenuItem with an action to show a dialog
+        MenuItem editStudent = MenuFactory.createMenuItem("Edit Student", () -> showFormDialog());
+
+        // Create "Exit" MenuItem with an action to close the application
+        MenuItem exitItem = MenuFactory.createMenuItem("Exit", () -> primaryStage.close());
+
+        // Add items to the "student" menu
+        MenuFactory.addItemsToMenu(studentMenu, Arrays.asList(addStudent, editStudent, exitItem));
+
+        // Add the "student" menu to the menu bar
+        menuBar.getMenus().add(studentMenu);
+
+        StudentFormGenerator newStudentForm = new StudentFormGenerator();
+
+        newStudentForm.configureActionButton("Add Student", event -> {
+            System.out.println("Form submitted!");
+        });
+
+        GridPane formPane = newStudentForm.createForm();
+
+        root.setCenter(formPane);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void showFormDialog() {
+        // Optionally prepopulate the form
+        // formGenerator.prepopulateForm("John", "Doe", "123 Main St", "12345", "NY");
+
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    static void CreateAddStudentScene(Stage primaryStage) {
-
     }
 }
