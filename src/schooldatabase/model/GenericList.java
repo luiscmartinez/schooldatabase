37709acaa79f62502af47ddeb7bf1c470bd7f2ec
Luis Martinez <mrluismartinezzz@gmail.com>
2024-04-02 22:30:10 -0700

    initial GenericList class

---
+++ src/schooldatabase/model/GenericList.java
@@ -0,0 +1,67 @@
+package schooldatabase.model;
+
+interface Comparable<T> {
+    int compareTo(T o);
+}
+
+public class GenericList<T extends Comparable<T>> {
+
+    @SuppressWarnings("hiding")
+    private class Node<T> {
+        T value;
+        Node<T> next;
+
+    }
+
+    private Node<T> first = null;
+    int count = 0;
+
+    public void add(T element) {
+        Node<T> newNode = new Node<T>();
+        newNode.value = element;
+        newNode.next = null;
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node<T> lastNode = goToLastNode(first);
+            lastNode.next = lastNode;
+        }
+        count++;
+    }
+
+    private Node<T> goToLastNode(Node<T> nodePointer) {
+        if (nodePointer == null) {
+            return nodePointer;
+
+        } else {
+            if (nodePointer.next == null) {
+                return nodePointer;
+            }
+
+            else {
+                return goToLastNode(nodePointer.next);
+            }
+
+        }
+
+    }
+
+    public T get(int pos)
+
+    {
+        Node<T> Nodeptr = first;
+        int hopCount = 0;
+        while (hopCount < count && hopCount < pos) {
+            if (Nodeptr != null) {
+                Nodeptr = Nodeptr.next;
+            }
+            hopCount++;
+        }
+        return Nodeptr.value;
+
+    }
+
+    public int size() {
+        return count;
+    }
+}
